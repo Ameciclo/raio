911a2bed3d2a8437e054a376ec16a0ce8597cbe3
Italo Sergio <italosergio@graduate.org>
2023-09-18 17:40:12 -0300

    feat: Footer component created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../images/logo1.png'
+
+function Footer() {
+
+  return (
+    <footer>
+      <img src={logo} className='' alt='LOAClima Logo'></img>
+      <aside>
+        <a href="/contato">Contato</a>
+        <a href="/faq">FAQ</a>
+        <a href="https://dados.ameciclo.org/observatorio">Observatório</a>
+      </aside>
+      <aside>
+        <a href="https://dados.ameciclo.org/">Dados da Ameciclo</a>
+        <a href="/contact">Notícias</a>
+        <a href="/https://www.ameciclo.org">Site da Ameciclo</a>
+      </aside>
+    </footer>
+  );
+}
+
+export default Footer;
