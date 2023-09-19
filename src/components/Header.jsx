a453078a26db6c90b3516902ebbc4d529795c042
Italo Sergio <italosergio@graduate.org>
2023-09-19 02:52:24 -0300

    feat: scrolling effect on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import logo from '../images/logo1.png'
 import { Link } from 'react-router-dom';
 
 function Header(props) {
+  const [headerBackground, setHeaderBackground] = useState('');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setHeaderBackground('scroll-efect');
+      } else {
+        setHeaderBackground('');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={`header fade-in ${props.page === "/news" && "header-news"}`}>
+    <header className={`header ${headerBackground} fade-in header-${props.page}`}>
       <div className='header-title'>
         <Link to="/" className='logo-link'>
           <img src={logo} alt="Logo do titulo" className='logo' />
