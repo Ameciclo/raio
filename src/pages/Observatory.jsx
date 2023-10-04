d3cc159183a3a678f91f41a384f38c95b26131fa
Italo Sergio <italosergio@graduate.org>
2023-10-04 10:31:19 -0300

    feat: set and implement request to Pernambuco open data

--- src/pages/Observatory.jsx
+++ src/pages/Observatory.jsx
@@ -4,9 +4,11 @@ import SideNavBar from '../components/SideNavBar';
 import Footer from '../components/Footer';
 import ActionCarousel from '../components/ActionsCarousel';
 import LazyLoad from 'react-lazyload';
+import { getActionsData } from '../services/request';
 
 function Observatory() {
   const [renderOthers, setRenderOthers] = useState(false);
+  const [actions, setActions] = useState([])
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -15,173 +17,15 @@ function Observatory() {
     return () => clearTimeout(timeout);
   }, []);
 
-  const actions = [
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0095 - ATUAÇÃO PARLAMENTAR",
-      "vlrdotatualizada": 0.00,
-      "vlrtotalpago": 47310.95,
-      "cd_nm_acao": "3539 - PAGAMENTO DE VERBA INDENIZATÓRIA AOS DEPUTADOS",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 0.00,
-      "vlrliquidado": 0.00,
-      "cd_nm_subfuncao": "031 - AÇÃO LEGISLATIVA"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0937 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DA ASSEMBLÉIA LEGISLATIVA DO ESTADO DE PERNAMBUCO - ALEPE",
-      "vlrdotatualizada": 22422900.00,
-      "vlrtotalpago": 21282702.54,
-      "cd_nm_acao": "2521 - PAGAMENTO DE VERBA INDENIZATÓRIA AOS SERVIDORES DA ASSEMBLEIA LEGISLATIVA DO ESTADO DE PERNAMBUCO  - ALEPE",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 21462000.00,
-      "vlrliquidado": 21282702.54,
-      "cd_nm_subfuncao": "031 - AÇÃO LEGISLATIVA"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0256 - CONTROLE EXTERNO DA ADMINISTRAÇÃO PÚBLICA ESTADUAL E MUNICIPAL",
-      "vlrdotatualizada": 278575800.00,
-      "vlrtotalpago": 275409042.54,
-      "cd_nm_acao": "1111 - CONTROLE EXTERNO DA APLICAÇÃO DOS RECURSOS PÚBLICOS DO ESTADO E DOS MUNICÍPIOS DE PERNAMBUCO",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 278122773.33,
-      "vlrliquidado": 275272496.08,
-      "cd_nm_subfuncao": "032 - CONTROLE EXTERNO"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0248 - CAPACITAÇÃO PARA O APRIMORAMENTO DA ADMINISTRAÇÃO PÚBLICA",
-      "vlrdotatualizada": 10000.00,
-      "vlrtotalpago": 0.00,
-      "cd_nm_acao": "3685 - CONSERVAÇÃO DO PATRIMÔNIO PÚBLICO NA ESCOLA DE CONTAS PÚBLICAS PROFESSOR BARRETO GUIMARÃES",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 0.00,
-      "vlrliquidado": 0.00,
-      "cd_nm_subfuncao": "122 - ADMINISTRAÇÃO GERAL"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0937 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DA ASSEMBLÉIA LEGISLATIVA DO ESTADO DE PERNAMBUCO - ALEPE",
-      "vlrdotatualizada": 6020000.00,
-      "vlrtotalpago": 0.00,
-      "cd_nm_acao": "3426 - IMPLEMENTAÇÃO DE POLÍTICA DE CONTENÇÃO DE DESPESAS E RESPONSABILIDADE AMBIENTAL",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 0.00,
-      "vlrliquidado": 0.00,
-      "cd_nm_subfuncao": "122 - ADMINISTRAÇÃO GERAL"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0937 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DA ASSEMBLÉIA LEGISLATIVA DO ESTADO DE PERNAMBUCO - ALEPE",
-      "vlrdotatualizada": 32820700.00,
-      "vlrtotalpago": 546200.33,
-      "cd_nm_acao": "4012 - ADEQUAÇÃO DAS INSTALAÇÕES FÍSICAS DA ASSEMBLEIA LEGISLATIVA DO ESTADO DE PERNAMBUCO - ALEPE",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 533662.54,
-      "vlrliquidado": 424168.36,
-      "cd_nm_subfuncao": "122 - ADMINISTRAÇÃO GERAL"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0937 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DA ASSEMBLÉIA LEGISLATIVA DO ESTADO DE PERNAMBUCO - ALEPE",
-      "vlrdotatualizada": 504220700.00,
-      "vlrtotalpago": 427291665.32,
-      "cd_nm_acao": "4353 - GESTÃO DAS ATIVIDADES DA ASSEMBLEIA LEGISLATIVA DO ESTADO DE PERNAMBUCO - ALEPE",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 469075574.08,
-      "vlrliquidado": 428951267.10,
-      "cd_nm_subfuncao": "122 - ADMINISTRAÇÃO GERAL"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0991 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "vlrdotatualizada": 8759300.00,
-      "vlrtotalpago": 1704995.15,
-      "cd_nm_acao": "4034 - CONSERVAÇÃO PATRIMONIAL DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 2697757.12,
-      "vlrliquidado": 1504392.70,
-      "cd_nm_subfuncao": "122 - ADMINISTRAÇÃO GERAL"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0991 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "vlrdotatualizada": 77926712.06,
-      "vlrtotalpago": 72848911.78,
-      "cd_nm_acao": "4411 - GESTÃO DAS ATIVIDADES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO  - TCE-PE",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 76099382.76,
-      "vlrliquidado": 70861095.84,
-      "cd_nm_subfuncao": "122 - ADMINISTRAÇÃO GERAL"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0991 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "vlrdotatualizada": 8571800.00,
-      "vlrtotalpago": 7684804.98,
-      "cd_nm_acao": "4411 - GESTÃO DAS ATIVIDADES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO  - TCE-PE",
-      "cd_nm_subacao": "1980 - CAPACITAÇÃO E VALORIZAÇÃO DE GESTORES E SERVIDORES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "vlrempenhado": 7939225.21,
-      "vlrliquidado": 7719375.12,
-      "cd_nm_subfuncao": "122 - ADMINISTRAÇÃO GERAL"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0991 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "vlrdotatualizada": 800600.00,
-      "vlrtotalpago": 707061.02,
-      "cd_nm_acao": "4411 - GESTÃO DAS ATIVIDADES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO  - TCE-PE",
-      "cd_nm_subacao": "1981 - ESTABELECIMENTO DE PARCERIAS PARA REALIZAÇÃO DE PESQUISAS E ASSISTÊNCIA TÉCNICA",
-      "vlrempenhado": 707061.02,
-      "vlrliquidado": 707061.02,
-      "cd_nm_subfuncao": "122 - ADMINISTRAÇÃO GERAL"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0937 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DA ASSEMBLÉIA LEGISLATIVA DO ESTADO DE PERNAMBUCO - ALEPE",
-      "vlrdotatualizada": 15483700.00,
-      "vlrtotalpago": 3467350.20,
-      "cd_nm_acao": "4249 - MANUTENÇÃO DA TECNOLOGIA DE INFORMAÇÃO E COMUNICAÇÃO DA ALEPE",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 4225838.20,
-      "vlrliquidado": 3493750.20,
-      "cd_nm_subfuncao": "126 - TECNOLOGIA DA INFORMAÇÃO"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0991 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "vlrdotatualizada": 30409200.00,
-      "vlrtotalpago": 26094873.08,
-      "cd_nm_acao": "2799 - MANUTENÇÃO DA TECNOLOGIA DE INFORMAÇÃO E COMUNICAÇÃO DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 28060975.93,
-      "vlrliquidado": 24979161.87,
-      "cd_nm_subfuncao": "126 - TECNOLOGIA DA INFORMAÇÃO"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0991 - APOIO GERENCIAL E TECNOLÓGICO ÀS AÇÕES DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "vlrdotatualizada": 507200.00,
-      "vlrtotalpago": 330608.13,
-      "cd_nm_acao": "2799 - MANUTENÇÃO DA TECNOLOGIA DE INFORMAÇÃO E COMUNICAÇÃO DO TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "cd_nm_subacao": "0185 - MANUTENÇÃO DA REDE DIGITAL CORPORATIVA DE GOVERNO - TRIBUNAL DE CONTAS DO ESTADO DE PERNAMBUCO - TCE-PE",
-      "vlrempenhado": 440278.75,
-      "vlrliquidado": 302882.37,
-      "cd_nm_subfuncao": "126 - TECNOLOGIA DA INFORMAÇÃO"
-    },
-    {
-      "cd_nm_funcao": "01 - LEGISLATIVA",
-      "cd_nm_prog": "0050 - EDUCAÇÃO PARA CIDADANIA NA ESCOLA DO LEGISLATIVO",
-      "vlrdotatualizada": 2717600.00,
-      "vlrtotalpago": 833152.98,
-      "cd_nm_acao": "0891 - REALIZAÇÃO DE CAPACITAÇÕES PARA GESTORES, SERVIDORES PÚBLICOS E CIDADÃOS",
-      "cd_nm_subacao": "0000 - OUTRAS MEDIDAS",
-      "vlrempenhado": 885248.99,
-      "vlrliquidado": 833152.98,
-      "cd_nm_subfuncao": "128 - FORMAÇÃO DE RECURSOS HUMANOS"
-    },
-  ];
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getActionsData();
+      console.log(data)
+      setActions(data);
+    }
+
+    return fetchData();
+  }, []);
 
   return (
     <>
--- src/services/request.js
+++ src/services/request.js
@@ -20,8 +20,9 @@ async function newsApi() {
   } catch (error) {
     console.log(error)
     alert(`
-    Recarregue a pagina, se esse erro persistir entre em contato!
-     erro:${error}
+    Erro em pegar os dados de notícias: ${error}
+    Recarregue a página!
+    Se esse erro persistir entre em contato!
     `);
     return [];
   }
@@ -35,25 +36,49 @@ async function singleNewsApi(id) {
   } catch (error) {
     console.log(error)
     alert(`
-    Recarregue a pagina, se esse erro persistir entre em contato!
-     erro:${error}
+    Erro em pegar os dados da notícia: ${error}
+    Recarregue a página!
+    Se esse erro persistir entre em contato!
     `);
     return [];
   }
 };
 
-async function faqApi(id) {
+async function faqApi() {
   try {
     const response = await axios.get('https://cms.ameciclo.org/faqs');
     return response.data;
   } catch (error) {
     console.log(error)
     alert(`
-    Recarregue a pagina, se esse erro persistir entre em contato!
-     erro:${error}
+    Erro em pegar os dados de dúvidas frequentes: ${error}
+    Recarregue a página!
+    Se esse erro persistir entre em contato!
     `);
     return [];
   }
 };
 
-export { newsApi, singleNewsApi, faqApi, homeContentAPI };
+async function getActionsData() {
+  try {
+    const response = await axios.get('https://dados.pe.gov.br/dataset/38401a88-5a99-4b21-99d2-2d4a36a241f1/resource/0a2e8fd7-7a65-46df-bd1b-15f2dfaaded7/download/acoes_e_programas_json_2021_20211231.json');
+    console.log(response)
+    return response.campos;
+  } catch (error) {
+    console.log(error)
+    alert(`
+    Erro em pegar os dados abertos de Pernambuco: ${error}
+    Recarregue a página!
+    Se esse erro persistir entre em contato!
+    `);
+    return [];
+  }
+};
+
+export {
+  newsApi,
+  singleNewsApi,
+  faqApi,
+  homeContentAPI,
+  getActionsData,
+};
