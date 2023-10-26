71b273f070d36eca2fdd3885b40c2a0c81b423a4
Italo Sergio <italosergio@graduate.org>
2023-10-25 21:45:02 -0300

    feat: Implement Proposals BE at Proposals Page

--- src/pages/Proposals.jsx
+++ src/pages/Proposals.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import SideNavBar from '../components/SideNavBar';
 import Footer from '../components/Footer';
 import ProposalsCard from '../components/ProposalsCard';
+import { getProposalsData } from '../services/request';
 
 function Proposals() {
-  const dataProposals = [
-    {id: 1, title: 'Plano Diretor Cicloviário da Região Metropolitana do Recife implementado nos próximos 4 anos!!', text: 'Implementar, nos próximos 4 anos, o Plano Diretor Cicloviário da Região Metropolitana do Recife. Lançado em 2014, foi construído pelo Governo do Estado, em parceria com as prefeituras e conta com o apoio da sociedade civil. É uma forma de diminuir mortes no trânsito, custos nos hospitais, os engarrafamentos e os efeitos das mudanças climáticas. São 600km de ciclovias, das quais apenas 100km foram implementada, e 20 mil vagas bicicletários, um investimento de R$700 mi a valores de hoje.'},
-    {id: 2, title: 'Destinar pelomenos 85% dos recursos de mobildiade para caminhar, pedalar e pegar busão!', text: 'Pessoas que caminham, pedalam e usam o transporte coletivo (a mobilidade sustentável) correspondem a cerca de 85% dos deslocamentos para o trabalho e escola, conforme pesquisa da Origem e Destino da RMR. Historicamente, a maior parte dos recursos são destinados à obras para carro e moto, que causam mortes e poluição. A mobilidade sustentável é 85% do trânsito, queremos NO MÍNIMO 85% dos recursos de mobilidade para calçadas, ciclovias, faixas exclusivas, segurança viária e redução das tarifas!'},
-  ]
+  const [dataProposals, setDataProposals] = useState([
+    {id: 1, title: ''},
+    {id: 2, title: ''},
+  ])
+
+  const requestProposalsData = async () => {
+    const data = await getProposalsData()
+    console.log(data)
+    setDataProposals(data)
+  }
+
+  useEffect(()=>{
+    requestProposalsData()
+  }, [])
 
   return (
     <>
