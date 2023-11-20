e17b41e6d87e4bdceb6ee145cbb2ca1a4d75b726
Italo Sergio <italosergio@graduate.org>
2023-11-19 21:03:32 -0300

    feat: News Maintenance Page

--- src/components/NewsCardMaintenance.jsx
+++ src/components/NewsCardMaintenance.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 
-function NewsCard(props) {
-  const news = props.data
+function NewsCardMaintenance() {
   const date = new Date();
   return (
-    <a href={`/noticias/${news.id}`} className='news-card'>
-      <img src={`${news.attributes.url}`} alt={news.attributes.imgDescription} />
+  <a href='/noticias/' className='news-card'>
+      <img src={"https://www.saofranciscodogloria.mg.leg.br/imagens/imagem_neutra_noticias.jpg"} alt="{news.attributes.imgDescription}" />
       <div className='news-card-text'>
         <div className='news-card-text-gradient'></div>
         <span>{date.getDate()}/{date.getMonth()}/{date.getFullYear()}</span>
         <div className='news-card-title'>
           <h1>Manutenção....</h1>
         </div>
-        <p>O serviço de notícias LOAClima está temporariamente em manutenção... Voltaremos em logo breve!</p>
+        <p>O serviço de notícias LOAClima está em manutenção agora... Voltaremos em breve!</p>
       </div>
     </a>
   );
 }
 
-export default NewsCard;
+export default NewsCardMaintenance;
